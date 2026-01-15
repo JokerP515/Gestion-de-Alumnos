9b47cc635506096fee322d16dd8ce1cd9efd7c84
JokerP515 <josedavid2010d@gmail.com>
2026-01-15 16:19:36 -0500

    Initial Spring project with Java 17

---
+++ src/main/java/com/jokerp515/gestionalumnos/GestionAlumnosApiApplication.java
@@ -0,0 +1,13 @@
+package com.jokerp515.gestionalumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAlumnosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAlumnosApiApplication.class, args);
+	}
+
+}
